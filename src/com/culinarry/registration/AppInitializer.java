aaf9ea296fcc56c51ef09fcf6de56fbc5928f956
isneha44 <isneha44@gmail.com>
2024-11-26 02:53:07 +0530

    did further crud operations and project Setup

--- src/com/culinarry/registration/AppInitializer.java
+++ src/com/culinarry/registration/AppInitializer.java
@@ -23,15 +23,35 @@ public class AppInitializer {
      // every codes and implementation start from here //
 
 //        To Save
+
 //        session.save(student);
 
 //        To Update
+
 //        student.setId(Long.valueOf("12"));
 //        session.update(student);
 
 //        To Get One Customer Object
-        Student student1 = session.get(Student.class, Long.valueOf(12));
-        System.out.println(student1);
+
+//        Student student1 = session.get(Student.class, Long.valueOf(12));
+//        System.out.println(student1); // TO PRINT ONE OBJECT
+//        System.out.println(student1.getEmail()); //TO PRINT ONE PROPERTY/ATTRIBUTE OR one thing FROM THE OBJECT
+//        System.out.println(student1.getEmail() + " - " + student1.getFirstName()); //TO PRINT MULTIPLE THINGS FROM THE OBJECT whatever THE WAY I WANT
+
+        // To Delete Customer
+
+             //1st way(NOT WORKING FOR ME NOW)
+
+//        Student student2 = new Student();
+//        student2.setId(Long.valueOf(11));
+//
+//        session.delete(student2);
+
+
+//            //2nd way(WORKING)
+//
+//        Student student3 = session.get(Student.class , Long.valueOf(12));
+//        session.delete(student3);
 
      transaction.commit();
      session.close();

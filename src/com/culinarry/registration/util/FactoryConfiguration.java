44cca4867939c6f78596760c5ef42736a44e85a9
isneha44 <isneha44@gmail.com>
2024-11-25 11:42:22 +0530

    Created Session Factory and configured it

--- src/com/culinarry/registration/util/FactoryConfiguration.java
+++ src/com/culinarry/registration/util/FactoryConfiguration.java
@@ -1,4 +1,28 @@
 package com.culinarry.registration.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+
+    private FactoryConfiguration() {
+        Configuration configuration = new Configuration();
+        sessionFactory = configuration.buildSessionFactory();
+
+    }
+
+    public FactoryConfiguration getInstance() {
+        return (factoryConfiguration == null) ? factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+
+
 }

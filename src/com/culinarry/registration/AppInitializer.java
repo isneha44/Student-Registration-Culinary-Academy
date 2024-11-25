f428001d8fe074f7d1e51e031bb2c62a86d491f9
isneha44 <isneha44@gmail.com>
2024-11-25 13:11:03 +0530

    Setuped hibernate successfully and insert data into database Student table

--- src/com/culinarry/registration/AppInitializer.java
+++ src/com/culinarry/registration/AppInitializer.java
@@ -1,19 +1,29 @@
 package com.culinarry.registration;
 
 import com.culinarry.registration.entity.Student;
+import com.culinarry.registration.util.FactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.Date;
 
 public class AppInitializer {
     public static void main(String[] args) {
      Student student = new Student();
-     student.setFirstName("Shashika");
-     student.setLastName("Madushan");
-     student.setEmail("shashi@gmail.com");
-     student.setPhoneNumber("+947134789456");
+     student.setId(student.getId());
+     student.setFirstName("Vihanga");
+     student.setLastName("Samanala");
+     student.setEmail("vihanga@gmail.com");
+     student.setPhoneNumber("+94745617997");
 
+     Session session = FactoryConfiguration.getInstance().getSession();
+
+     Transaction transaction = session.beginTransaction();
 
      session.save(student);
+
+     transaction.commit();
+     session.close();
 
     }
 
--- src/com/culinarry/registration/util/FactoryConfiguration.java
+++ src/com/culinarry/registration/util/FactoryConfiguration.java
@@ -1,5 +1,9 @@
 package com.culinarry.registration.util;
 
+import com.culinarry.registration.entity.Program;
+import com.culinarry.registration.entity.Registration;
+import com.culinarry.registration.entity.Student;
+import com.culinarry.registration.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,12 +14,14 @@ public class FactoryConfiguration {
 
 
     private FactoryConfiguration() {
-        Configuration configuration = new Configuration();
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Program.class).addAnnotatedClass(Registration.class).addAnnotatedClass(User.class);
+        
+        
         sessionFactory = configuration.buildSessionFactory();
 
     }
 
-    public FactoryConfiguration getInstance() {
+    public static FactoryConfiguration getInstance() {
         return (factoryConfiguration == null) ? factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
     }
 
